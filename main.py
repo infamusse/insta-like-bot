78694e5704e0efabc290dd9326fd362375668258
mjalowiczor <m.jalowiczor@hotmail.com>
2020-03-22 15:22:43 +0100

    add loop for photos link, and like photo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from time import sleep
 
 class InstaBot:
-    def __init__(self, username, pw):
+    def __init__(self, username, password):
         self.driver = webdriver.Chrome(executable_path="C:\Program Files\chromedriver\chromedriver.exe")
         self.username = username
         self.driver.get("https://instagram.com")
@@ -11,7 +11,7 @@ class InstaBot:
             .send_keys(username)
         self.driver.find_element_by_xpath(
             "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input") \
-            .send_keys(pw)
+            .send_keys(password)
         self.driver.find_element_by_xpath(
             "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[4]/button") \
             .click()
@@ -26,15 +26,24 @@ class InstaBot:
             .click()
 
 
-    def make_like(self, hashtag):
+    def like_photo(self, hashtag):
         self.driver.get("https://www.instagram.com/explore/tags/{}/".format(hashtag))
-        # self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input")\
-        #     .send_keys(hashtag)
-        # sleep(3)
-        # self.driver.find_element_by_xpath("//a[contains(@href, '/#{}')]".format(hashtag)) \
-        #     .click()
-        # hash_menu = self.driver.find_element_by_xpath("/html/body/div[1]/section/nav/div[2]/div/div/div[2]/div[2]/div[2]/div")
-        # print(hash_menu)
+        sleep(2)
+        for i in range(1, 2):
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            sleep(2)
 
-my_bot = InstaBot("login", "password")
-my_bot.make_like('canolli')
+        # searching for photo link
+        hrefs = self.driver.find_elements_by_css_selector("article a")
+        pic_hrefs = [elem.get_attribute('href') for elem in hrefs]
+
+        for index, href in enumerate(pic_hrefs):
+            self.driver.get(href)
+            like_button = self.driver.find_element_by_css_selector("section > span:first-child > button")
+            sleep(1)
+            like_button.click()
+            sleep(1)
+
+
+my_bot = InstaBot("login ", "password")
+my_bot.like_photo('hashtag')
